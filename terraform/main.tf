ba9a403d4538c56fd25053667bbd3dec18dee8f9
Mess-big <mesfin.yimamy@gmail.com>
2026-08-22 19:53:31 -0400

    Add public subnet internet routing

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   region  = "us-east-1"
-  profile = "mess-bg-lab-bootstrap"
+  #profile = "mess-bg-lab-bootstrap"
 }
 resource "aws_vpc" "security_lab" {
   cidr_block           = "10.10.0.0/16"
